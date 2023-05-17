929c3cca820e60e496cbbb1e7945f42707ea7635
Evgeny Pospelov <pospelov.genya@yandex.ru>
2023-05-17 19:09:59 +0500

    add page with Main Info

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,20 +1,46 @@
 <template>
-    <Page class="page">
-        <ActionBar class="ab">
-            <Label text="Networking" class="header"/>
-        </ActionBar>
+  <Page class="page">
+    <ActionBar class="ab">
+      <Label text="Networking" class="header" />
+    </ActionBar>       
+     
+    <GridLayout rows="*, auto, auto, *, *" columns="*">
+      <!-- <Label row="0" :text="show" textWrap="true" /> -->
+      <TextField row="1" hint="ipv4 adress input" v-model="ip" />
+      <Button row="2" text="Fint this ip" @tap="find()" />
+      <!-- <Label row="3" :text="qe" textWrap="true" /> -->
         
-        <Label text="hello" />
-
-    </Page>
+    </GridLayout>
+    
+  </Page>
 </template>
-
+    
 <script>
+import MainData from './MainData.vue';
 export default {
   data () {
-    return {};
+    return {
+      ip: '5.141.28.183',
+      qe: 0,
+      show: '-10',
+    };
   },
   methods: {
+    async find() {
+      const response = await fetch("https://api.shodan.io/shodan/host/" + this.ip + "?key=X9bo1JfYKqcrKIzslhW90OzH3VagvbTW");
+      const req = await response.json();
+      this.$navigateTo(MainData, {props: {req: req}});
+      
+      
+      // this.show = await req;
+      // const req = await response.text();
+      // const response2 = await fetch("https://api.shodan.io/shodan/host/8.8.8.8?key=X9bo1JfYKqcrKIzslhW90OzH3VagvbTW");
+      // const eq = await response2.json();
+      // this.qe = await response.text();
+      // this.qe = await Object.keys(eq);
+      // var req = Http.getString("https://api.shodan.io/shodan/host/8.8.8.8?key=X9bo1JfYKqcrKIzslhW90OzH3VagvbTW")
+      // var req = Http.getJSON('https://api.shodan.io/shodan/host/' + String(this.ip) + '?key=X9bo1JfYKqcrKIzslhW90OzH3VagvbTW');
+    },
   }
 };
 </script>
---
+++ app/components/MainData.vue
@@ -0,0 +1,27 @@
+<template>
+    <Page class="page">
+        <ActionBar class="ab">
+          <Label text="Main Info" class="header" />
+        </ActionBar>
+        <FlexboxLayout flexDirection="column" >
+            <!-- <Label :text="req" textWrap="true" /> -->
+            <Label :text="item + ': ' + req[item]" textWrap="true" v-for="item in Object.keys(req)" />
+        </FlexboxLayout>
+    </Page>
+        
+</template>
+    
+<script>
+export default {
+  props: ['req'],
+  data () {
+    return {};
+  },
+  methods: {
+  }
+};
+</script>
+    
+<style scoped lang="scss">
+
+</style>

5a238a494397ca839216cfeb2139effd7bcbb9fe
Evgeny Pospelov <pospelov.genya@yandex.ru>
2023-05-18 19:45:38 +0500

    add ip validation,LoadIndicator and update history

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -5,12 +5,14 @@
     </ActionBar>       
      
     <GridLayout rows="*, auto, auto, *, auto, *" columns="*">
-      <TextField row="1" hint="ipv4 adress input" v-model="ip" />
-      <Button row="2" text="Find this ip" @tap="find()" />
+      <ActivityIndicator row="0" :busy="loadData" />
+
+      <TextField row="1" v-model="ip" class="textbar" />
+      <Label row="2" text="Find this ip" @tap="find()" class="button"/>
 
 			<FlexboxLayout row="4" flexDirection="column">
         <Label text="Search history:" textWrap="true" width="50%" class="datablock" />
-				<Label :text="item.ip" textWrap="true" width="50%" itemWidth="50%" v-for="(item, key) in history" @key="key" @tap="findhist(key)" class="button" />
+				<Label :text="item" textWrap="true" width="50%" itemWidth="50%" v-for="(item, key) in history" @key="key" @tap="findhist(key)" class="button" />
 			</FlexboxLayout>
     </GridLayout>
     
@@ -21,45 +23,56 @@
 import * as ApplicationSettings from '@nativescript/core/application-settings';
 import Main from "./MainData.vue"
 export default {
-  data () {
+  data() {
     return {
-      ip: '5.141.28.183',
-			history: [],
+      ip: '',
+      history: [],
+      show: {},
+      loadData: false,
+      regex: /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/,
     };
   },
-	beforeMount() {
-      if (ApplicationSettings.getString("history")) {
-        this.history = Object.values(JSON.parse(ApplicationSettings.getString("history")));
-      }
-		},
+  beforeMount() {
+    if (ApplicationSettings.getString("history")) {
+      this.history = ApplicationSettings.getString("history").split(',') ;
+    }
+  },
   methods: {
     async find() {
+      if (!(this.regex.test(this.ip))){
+        return;
+      }
+      this.loadData = true;
       const response = await fetch("https://api.shodan.io/shodan/host/" + this.ip + "?key=X9bo1JfYKqcrKIzslhW90OzH3VagvbTW");
       const req = await response.json();
-			if (this.history.length < 5) {
-				this.history.push({ip: this.ip});
-			}
-			else {
-				this.history.splice(0, 1);
-				this.history.push({ip: this.ip});
-			}
-			this.save(req);
+      if (!(this.history.includes(this.ip))) {
+        if (this.history.length < 5) {
+          this.history.push(this.ip);
+        }
+        else {
+          this.history.splice(0, 1);
+          this.history.push(this.ip);
+        }
+      }
+      await this.save(req);
       this.ToPage();
     },
-		async findhist(id) {
-			const response = await fetch("https://api.shodan.io/shodan/host/" + this.history[id].ip + "?key=X9bo1JfYKqcrKIzslhW90OzH3VagvbTW");
+    async findhist(id) {
+      this.loadData = true;
+      const response = await fetch("https://api.shodan.io/shodan/host/" + this.history[id] + "?key=X9bo1JfYKqcrKIzslhW90OzH3VagvbTW");
       const req = await response.json();
-			await this.save(req);
+      this.show = req;
+      await this.save(req);
       this.ToPage();
-		},
-		async save(req) {
-			ApplicationSettings.setString("history", JSON.stringify(Object.assign({}, this.history)));
-			ApplicationSettings.setString("req", JSON.stringify(Object.assign({}, req)));
-		},
-		ToPage() {
-			this.$navigateTo(Main);
-		}
-  }
+    },
+    async save(req) {
+      ApplicationSettings.setString("history", this.history.toString());
+      ApplicationSettings.setString("req", JSON.stringify(Object.assign({}, req)));
+    },
+    ToPage() {
+      this.$navigateTo(Main);
+    }
+  },
 };
 </script>
 
